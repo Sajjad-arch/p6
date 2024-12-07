418b636b46f405ed5e62281d5bbf3454aa6f46bc
Sajjad-arch <sajjadulislam870@gmail.com>
2024-12-07 22:33:05 +0600

    Add files via upload

---
+++ p6.c
@@ -0,0 +1,32 @@
+//Given any integer, character value i, print the character corresponding to it in the ASCII table.
+
+
+
+#include  <stdio.h>
+
+int main(void)
+
+{
+    int  i;
+    char c;
+    i=98;
+    c='d';
+
+
+    printf("%c %d", i,c);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
